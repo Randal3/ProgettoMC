b54819f6ba1caca008762ca70aeba56183bf0ff2
EmanueleC <emanuele.cannavo@outlook.it>
2020-11-13 14:55:23 +0100

    Logica_Indizio
    bottoni indizio + close

---
+++ ProgettoMC/Assets/Script/Close_Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Close_Button : MonoBehaviour
+{
+
+    public GameObject canvas_object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Close()
+    {
+        Image[] ts = canvas_object.gameObject.GetComponentsInChildren<Image>();
+        foreach (Image t in ts)
+        {
+            if (t != null && t.gameObject != null)
+                t.gameObject.GetComponent<Image>().enabled = false;
+        }
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ ProgettoMC/Assets/Script/Indizio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Indizio : MonoBehaviour
+{
+    public GameObject indizio;
+    public GameObject button_Indizio;
+    public GameObject button_Close;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            button_Indizio.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            button_Indizio.gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowIndizio()
+    {
+        indizio.gameObject.GetComponent<Image>().enabled = true;
+        button_Close.gameObject.SetActive(true);
+
+    }
+}
