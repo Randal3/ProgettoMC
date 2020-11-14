44bdf760a6fc43c2672037ce7288a9219b79a6f2
b3rserker9 <aliyoussef3c@gmail.com>
2020-11-14 11:49:32 +0100

    ok
    inseguimento mostro semi-completo  risolvere angoli che bloccano il mostro

---
+++ ProgettoMC/Assets/porte/door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    private Animator animator;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = target.position.y;
+        float x = target.position.x;
+        if (y > 3 && y < 9.5 && x > 20 && x < 24.2)
+        {
+            animator.SetBool("open", true);
+        }
+        else
+        {
+            animator.SetBool("open", false);
+        }
+    }
+}
---
+++ ProgettoMC/Assets/porte/doorM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorM : MonoBehaviour
+{
+    private Animator animator;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = target.position.y;
+        float x = target.position.x;
+        if (y > 10 && y < 19 && x > 20 && x < 24.2)
+        {
+            animator.SetBool("open", true);
+            gameObject.layer = 0;
+        }
+        else
+        {
+            animator.SetBool("open", false);
+            gameObject.layer = 8;
+        }
+    }
+}
