b39db477329a211510ebdf6c3d15e231a2c3bec4
b3rserker9 <aliyoussef3c@gmail.com>
2020-11-15 14:33:19 +0100

    inventario e chiave
    implementazione chiave e inventario(da migliorare la grafica :) )

---
+++ ProgettoMC/Assets/inventory/InventoryMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject Inventory;
+    [SerializeField] private bool activate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)){
+            activate = !activate;
+        }
+        if (activate)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+    void ActivateMenu()
+    {
+        Inventory.SetActive(true);
+    }
+    void DeactivateMenu()
+    {
+        Inventory.SetActive(false);
+    }
+}
