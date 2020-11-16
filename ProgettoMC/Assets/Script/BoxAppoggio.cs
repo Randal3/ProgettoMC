c01f6970ebde8a964b0c8d2102efe49281e9ca86
RiccardoB <riccardo.brucato@outlook.it>
2020-11-16 12:30:18 +0100

    TESTIdnovinello

---
+++ ProgettoMC/Assets/Script/BoxAppoggio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxAppoggio : MonoBehaviour
+{
+    public Transform holder;
+    public string Tag;
+    public string nuovoTag;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holder.transform.parent == null)
+        {
+            Debug.Log("La chiave ci sta");
+        }
+        else
+        {
+            Debug.Log("La chiave NO");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Tag))
+        {
+            collision.gameObject.tag = nuovoTag;
+            collision.gameObject.transform.parent = holder;
+            collision.gameObject.transform.position = holder.position;
+        }
+    }
+}
---
+++ ProgettoMC/Assets/Script/Grab.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("CIAO");
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+
+        if(grabCheck.collider != null && grabCheck.collider.CompareTag("ChiaveAttiva"))
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+            }
+            else
+            {
+                grabCheck.collider.gameObject.transform.parent = null;
+            }
+        }
+
+
+    }
+}
