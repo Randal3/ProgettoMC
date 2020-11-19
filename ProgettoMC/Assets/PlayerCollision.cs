bd52611dd9984e9616056e31a33c9b88542272b7
b3rserker9 <aliyoussef3c@gmail.com>
2020-11-19 22:33:40 +0100

    gameOver

---
+++ ProgettoMC/Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "enemy")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameOver>().EndGAme();
+        }
+    }
+}
