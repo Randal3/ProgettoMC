9f7f95c6614294eec72a989237ce8f5d71c7c579
Missi5 <alex.miss99@gmail.com>
2021-04-26 15:56:36 +0200

    lale indo

---
+++ ProgettoMC/Assets/DisplayIndo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayIndo : MonoBehaviour
+{
+    public bool playerInrange;
+    public GameObject game;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInrange = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInrange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInrange && Input.GetKey(KeyCode.B))
+        {
+            game.SetActive(true);
+        }
+    }
+}
