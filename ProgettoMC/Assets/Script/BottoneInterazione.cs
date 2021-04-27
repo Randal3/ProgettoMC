482441d913a1e11e3d7a38f674f12a5734feea31
Randal3 <riccardo.brucato@outlook.it>
2021-04-27 15:24:34 +0200

    PrimeModificheBottone

---
+++ ProgettoMC/Assets/Script/BottoneInterazione.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BottoneInterazione : MonoBehaviour
+{
+    public Transform boxDetect;
+    public Transform boxHolder;
+    public float rayDist;
+
+    public static event Action<string> ButtonPressed = delegate { };
+
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
+    }
+    private void ButtonClicked()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(boxDetect.position, Vector2.right * transform.localScale, rayDist);
+
+        if (grabCheck.collider != null && grabCheck.collider.CompareTag("ChiaveAttiva"))
+        {
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+
+        if (grabCheck.collider == null)
+        {
+            grabCheck.collider.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().isTrigger = false;
+            grabCheck.collider.gameObject.transform.parent = null;
+        }
+    }
+}
