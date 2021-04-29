cd498c37fbbdf1b92bc345087186fdf530c0b7ca
b3rserker9 <aliyoussef3c@gmail.com>
2021-04-29 18:25:27 +0200

    button complete

--- ProgettoMC/Assets/Return_button.cs
+++ ProgettoMC/Assets/Return_button.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Indizio_bottone : MonoBehaviour
+public class Return_button : MonoBehaviour
 {
     public GameObject game;
-    public GameObject but_ret; //pulsante di return
-    private GameObject joystick;
+    public GameObject joystick;
     public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        joystick = GameObject.FindGameObjectWithTag("joystick");
+        
         gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
     }
 
     private void ButtonClicked()
     {
-        but_ret.SetActive(true);
-        game.SetActive(true);
-        camera.enabled = false;
-        joystick.SetActive(false);
+        game.SetActive(false);
+        camera.enabled = true;
+        joystick.SetActive(true);
         gameObject.SetActive(false);
     }
-    }
+}
