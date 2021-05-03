1d7bfd5fe9b769ecf30c3040cf9be673eb28416c
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-03 17:08:11 +0200

    Main menu

---
+++ ProgettoMC/Assets/Mainmenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
