70600d06d262121f5a9157a8e8f98222d88b37d4
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-04 00:57:41 +0200

    miglioramenti

---
+++ ProgettoMC/Assets/EnemyGFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aipath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aipath.desiredVelocity.x <= 0.01f)
+        {
+            //transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (aipath.desiredVelocity.x >= -0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+}
