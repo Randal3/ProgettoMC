966c6b46d8e7f7617ad5cf37348cf30fad370ca8
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-04 13:30:38 +0200

    base del main menu

--- ProgettoMC/Assets/Volume.cs
+++ ProgettoMC/Assets/Volume.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Volume : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // public AudioMixer audiomixer;
+    public AudioSource Audio;
+
     void Start()
     {
-        
+        Audio = GetComponent<AudioSource>();
     }
 
+    public void SetVolume(float volume)
+    {
+        //audiomixer.SetFloat("volume", volume);
+        Audio.volume = volume;
+    }
+    
     // Update is called once per frame
     void Update()
     {
