f945724a267592276d0f7a2cd422ce0e8d6ec456
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-10 23:39:51 +0200

    movimento Enemy migliorata

--- ProgettoMC/Assets/Script/EnemyGFX.cs
+++ ProgettoMC/Assets/Script/EnemyGFX.cs
@@ -6,19 +6,33 @@ using Pathfinding;
 public class EnemyGFX : MonoBehaviour
 {
     public AIPath aipath;
-
+    public float speed = 5f;
     // Update is called once per frame
     void Update()
     {
-        if (aipath.desiredVelocity.x <= 0.01f)
+        Debug.Log(aipath.desiredVelocity);
+        /*if (aipath.desiredVelocity.x <= 0.01f)
         {
             //transform.localScale = new Vector3(-1f, 1f, 1f);
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
         }
-        else if (aipath.desiredVelocity.x >= -0.01f)
+        if (aipath.desiredVelocity.x >= -0.01f)
         {
             //transform.localScale = new Vector3(1f, 1f, 1f);
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
+         if (aipath.desiredVelocity.y >= 0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+         if (aipath.desiredVelocity.y <= -0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(0f, 0f, 270f);
+        }*/
+        float angle = Mathf.Atan2(aipath.desiredVelocity.y, aipath.desiredVelocity.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 }
