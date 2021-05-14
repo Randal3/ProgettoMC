5f2f5a63243a155713ff0724b1ebbd11ff0905cd
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-14 23:48:47 +0200

    puzzle bug

---
+++ ProgettoMC/Assets/Random_Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Puzzle : MonoBehaviour
+{
+    public Transform[] pictures;
+    private int[] angle = { 0, 90, 180, 270 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            Debug.Log(i);
+            pictures[i].Rotate(0,0, angle[Random.Range(0, 3)]);
+        }
+    }
+}
