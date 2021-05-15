b1154e9ddd5bedbabfc15306d2219ffb5ecd4dc5
Missi5 <alex.miss99@gmail.com>
2021-05-15 10:28:35 +0200

    mostro che scompare dopo t tempo

--- ProgettoMC/Assets/VanishEnemy.cs
+++ ProgettoMC/Assets/VanishEnemy.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class VanishEnemy : MonoBehaviour
 {
     public float time;
+    public GameObject enemy;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (gameObject.activeInHierarchy)
-        {
             time += Time.deltaTime;
             Debug.Log(time);
+        if (time >= 15)
+        {
+            animator.SetBool("death", true);
+            Invoke("Vanish", 2);
         }
+    }
+    void Vanish()
+    {
         time = 0;
-        
+        enemy.SetActive(false);
+        animator.SetBool("death", false);
     }
 }
