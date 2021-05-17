ad067f0860ad7c0f65b057f44c8061cf21e7726e
b3rserker9 <aliyoussef3c@gmail.com>
2021-05-17 12:59:43 +0200

    miglioramento mio puzzle

--- ProgettoMC/Assets/Script/Soluzione.cs
+++ ProgettoMC/Assets/Script/Soluzione.cs
@@ -26,13 +26,14 @@ public class Soluzione : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pictures[0].rotation.eulerAngles.z == 180 &&
-            (pictures[1].rotation.eulerAngles.z == 0 ||
-            pictures[1].rotation.eulerAngles.z == 180) &&
-            pictures[2].rotation.eulerAngles.z == 180 && 
-           (pictures[3].rotation.eulerAngles.z == 0 ||
-            pictures[3].rotation.eulerAngles.z == 270 ) &&
-            pictures[5].rotation.eulerAngles.z == 0)
+        Debug.Log("2:" + pictures[0].rotation.eulerAngles.z);
+        Debug.Log("4:" + pictures[1].rotation.eulerAngles.z);
+        if ((pictures[0].rotation.eulerAngles.z == 180 || pictures[0].rotation.eulerAngles.z <= 10) &&
+             pictures[1].rotation.eulerAngles.z == 0 &&
+             pictures[2].rotation.eulerAngles.z == 180 && 
+            (pictures[3].rotation.eulerAngles.z <= 10 || pictures[3].rotation.eulerAngles.z == 180 ) &&
+             pictures[4].rotation.eulerAngles.z == 180 &&
+             pictures[5].rotation.eulerAngles.z == 270)
         {
             youWin = true;
             game.SetActive(false);
