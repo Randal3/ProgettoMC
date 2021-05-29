3510bb795a27abf3f73082a62604804b12844e89
EmanueleC <emanuele.cannavo@outlook.it>
2021-05-29 17:34:28 +0200

    Cominciata implementazione classifica

---
+++ ProgettoMC/Assets/Script/PlayFabManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviour
+{
+    public GameObject rowPrefab;
+    public Transform rowsParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successo login");
+    }
+
+    public static void OnError(PlayFabError error)
+    {
+        Debug.Log("Errore");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    public static void SendLeaderboard(int time)
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "LowestTime", Value = time
+                }
+         }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+    }
+
+    public static void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("Successo classifica");
+    }
+
+    public void GetLeaderboard()
+    {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "LowestTime",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+
+    void OnLeaderboardGet(GetLeaderboardResult result)
+    {
+        foreach(Transform item in rowsParent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in result.Leaderboard)
+        {
+            GameObject newGo = Instantiate(rowPrefab, rowsParent);
+            Text[] texts = newGo.GetComponentsInChildren<Text>();
+            texts[0].text = (item.Position + 1).ToString();
+            texts[1].text = item.PlayFabId;
+            texts[2].text = item.StatValue.ToString();
+
+            Debug.Log(item.Position + item.PlayFabId + item.StatValue);
+        }
+
+    }
+}
---
+++ ProgettoMC/Assets/Script/tempoFinale.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class tempoFinale : MonoBehaviour
+{
+    public PlayFabManager pf;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string tempo = FineGioco.tempoFinale;
+        text.text = tempo;
+
+        string t_score = tempo;
+        string t_score1 = t_score.Replace(":", string.Empty);
+        string t_score2 = t_score1.Replace(",", string.Empty);
+        int score = System.Convert.ToInt32(t_score2);
+        PlayFabManager.SendLeaderboard(score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
