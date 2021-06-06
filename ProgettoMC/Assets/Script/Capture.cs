de9f85c367fc74576a7c547b958d1535572d21c2
Missi5 <alex.miss99@gmail.com>
2021-06-06 02:46:47 +0200

    condivisione con i social media

---
+++ ProgettoMC/Assets/Script/Capture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Capture : MonoBehaviour
+{
+    public Button screen;
+    void Start()
+    {
+        screen.onClick.AddListener(ScreenShot);
+    }
+    public void ScreenShot()
+    {
+        ScreenCapture.CaptureScreenshot("pippo.png", 2);
+        string destinazione = Path.Combine(Application.dataPath, "pippo.png");
+        Debug.Log(destinazione);
+    }
+}
