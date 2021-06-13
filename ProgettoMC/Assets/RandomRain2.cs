cf765314950027704c83fa06ddee52fcec21f151
b3rserker9 <aliyoussef3c@gmail.com>
2021-06-13 14:10:55 +0200

    complete rain

---
+++ ProgettoMC/Assets/RandomRain2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRain2 : MonoBehaviour
+{
+    public GameObject rain;
+    private GameObject rainclone;
+    public GameObject container;
+    int i = 0;
+    private float InstantiationTimer = 0.5f;
+    // Start is called before the first frame update
+    Vector2 randomVec;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        randomVec = new Vector2(Random.Range(29, 60), Random.Range(-57, -20));
+        rainclone = Instantiate(rain, randomVec, Quaternion.identity);
+        rainclone.transform.parent = container.transform;
+        rainclone.SetActive(true);
+        rainclone.name = "rainClone" + i;
+        //Invoke("RainDestroy", 0.2f);
+
+    }
+    /*  public void RainDestroy()
+      {
+
+              Debug.Log("fatto"+i);
+              Destroy(rainclone);
+              Debug.Log("fatto");
+
+      }*/
+}
