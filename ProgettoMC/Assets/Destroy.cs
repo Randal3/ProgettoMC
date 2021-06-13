1304e1aefb069b21ce5ee33a068590e59333b5f8
b3rserker9 <aliyoussef3c@gmail.com>
2021-06-13 02:05:19 +0200

    rain effect

---
+++ ProgettoMC/Assets/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+  
+    void Update()
+    {
+        Invoke("Rain", 0.5f);
+    }
+    public void Rain()
+    {
+        Debug.Log("distrutto");
+        Destroy(gameObject);
+    }
+}
---
+++ ProgettoMC/Assets/RamdomRain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamdomRain : MonoBehaviour
+{
+    public GameObject rain;
+    private GameObject rainclone;
+    int i=0;
+    private float InstantiationTimer = 0.5f;
+    // Start is called before the first frame update
+    Vector2 randomVec;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+            randomVec = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
+             rainclone = Instantiate(rain, randomVec, Quaternion.identity);
+        rainclone.SetActive(true);
+            rainclone.name = "rainClone" + i;
+        //Invoke("RainDestroy", 0.2f);
+        
+     }
+  /*  public void RainDestroy()
+    {
+
+            Debug.Log("fatto"+i);
+            Destroy(rainclone);
+            Debug.Log("fatto");
+        
+    }*/
+}
