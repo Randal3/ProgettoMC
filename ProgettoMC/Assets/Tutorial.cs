73e7c8fb9badda3930c84dcd978a93038570446a
b3rserker9 <aliyoussef3c@gmail.com>
2021-06-21 09:53:12 +0200

    Tutorial

---
+++ ProgettoMC/Assets/Tutorial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Main;
+    public GameObject Background;
+    // Update is called once per frame
+    void Awake()
+    {
+        Invoke("off", 17f);       
+    }
+    public void off()
+    {
+        Main.SetActive(true);
+        Background.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
