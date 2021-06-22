65b2f76b637683c26c0172ea23459063bfdbe596
b3rserker9 <aliyoussef3c@gmail.com>
2021-06-22 14:20:16 +0200

    Cuffie intro

--- ProgettoMC/Assets/Wait.cs
+++ ProgettoMC/Assets/Wait.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class Wait : MonoBehaviour
 {
-    public float wait = 5f;
+    public float wait_intro = 5f;
+    public float wait_cuffie = 4.5f;
+    public VideoPlayer cuffie;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(wait_for_intro());
+        StartCoroutine(Wait_for_intro());
     }
-    IEnumerator wait_for_intro()
+    IEnumerator Cuffie_intro()
     {
-        yield return new WaitForSeconds(wait);
+        cuffie.Play();
+        yield return new WaitForSeconds(wait_cuffie);
         SceneManager.LoadScene("Menu");
     }
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(wait_intro);
+        StartCoroutine(Cuffie_intro());
+    }
+    
 }
